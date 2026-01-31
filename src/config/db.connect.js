6bc912336c78d94e342f007111d4315e07c541d1
Md. Ahsan Khan <sk6582757@gmail.com>
2026-01-31 14:28:34 +0530

    added serverslectiontimoutms to 5seconds

--- src/config/db.connect.js
+++ src/config/db.connect.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 const dbconnect = async()=>{
 try{
-    await mongoose.connect(process.env.MONGO_URI)
+    await mongoose.connect(process.env.MONGO_URI,{
+        serverSelectionTimeoutMS:5000,
+    })
     console.log("db connected successfully")
 }
 catch(err){
